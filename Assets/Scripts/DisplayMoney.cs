b5c95bcc134ab1f081b93490bbad7c778508ab5f
gapatterson <gapattersonfortomorrow@gmail.com>
2017-12-13 15:31:12 -0500

    Race Skeleton

---
+++ Assets/Scripts/DisplayMoney.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DisplayMoney : MonoBehaviour {
+	Text txt;
+	private double money=50000;
+
+	void Start () {
+		txt = gameObject.GetComponent<Text> ();
+		txt.text = "Money: " + money;
+	}
+	
+
+	void Update () {
+		txt.text = "Mooney: " + money;
+	}
+
+	public double GetMoney(){
+		return money;
+	}
+
+	public void SpendMoney(double spendingAmount){
+		money -= spendingAmount;
+	}
+}
---
+++ Assets/Scripts/Race.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Race : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void Create_Race (Horse entryHorse){
+		List<Horse> horses = new List<Horse>();
+		horses.Add (entryHorse);
+		while(horses.Count < 6){
+			horses.Add(new Horse());
+		}
+	}
+
+	Horse Determine_Winner (List<Horse> horses){
+		return horses [0];
+	}
+}
